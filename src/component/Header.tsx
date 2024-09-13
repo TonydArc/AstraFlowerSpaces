8d8601c924eeca71927466fe5cfd8b0dce303035
TonydArc <tonyking497@gmail.com>
2024-09-13 16:52:06 +0700

    mini change

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -5,7 +5,7 @@ interface HeaderProps {
   path: string;
 }
 
-const Header: React.FC<HeaderProps> = ({ title, path }) => {
+const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
     <>
       {/* Header Start */}
@@ -13,8 +13,8 @@ const Header: React.FC<HeaderProps> = ({ title, path }) => {
         <div className="container text-center py-5" style={{ maxWidth: '900px' }}>
           <h3 className="text-white text-uppercase fw-bold display-3 mb-4">{title}</h3>
           <ol className="breadcrumb justify-content-center mb-0">
-            <li className="breadcrumb-item"><a href="/">Home</a></li>
-            <li className="breadcrumb-item active text-white">{path}</li>
+            {/* <li className="breadcrumb-item"><a href="/">Home</a></li>
+            <li className="breadcrumb-item active text-white">{path}</li> */}
           </ol>
         </div>
       </div>
